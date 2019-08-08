3bca205305a9407e6514221d9b82eebc6f7b1151
prgthees <51222680+prgthees@users.noreply.github.com>
2019-08-08 12:48:50 +0530

    Update prg114.py

--- prg114.py
+++ prg114.py
@@ -1,11 +1,11 @@
-p1,p2=map(str,input().split())
-r=0
-if len(p1)>len(p2):
-  p1,p2=p2,p1
-g=0
-while g<len(p1):
-  r+=(ord(p2[g])-ord(p1[g]))
-  g+=1
-for p in range(g,len(p2)):
-  r+=ord(p2[g])-ord('a')+1
-print(r)
+d1,d2=map(str,input().split())
+y=0
+if len(d1)>len(d2):
+  d1,d2=d2,d1
+p=0
+while p<len(d1):
+  y+=(ord(d2[p])-ord(d1[p]))
+  p+=1
+for p in range(p,len(d2)):
+  y+=ord(d2[p])-ord('a')+1
+print(y)
